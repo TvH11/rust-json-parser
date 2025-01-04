10e35a88eaf634c3e663a8ca61cab49d8711ccdb
TvH11 <T.vanHoof@student.han.nl>
2025-01-04 22:24:08 +0100

    added display for json enum

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,54 @@ mod tokenizer;
 mod parser;
 
 use std::collections::{HashMap};
-use std::{env, fs, io};
+use std::{env, fmt, fs, io};
+use std::fmt::Formatter;
 use std::process::exit;
 
 #[derive(Debug)]
-enum JsonValue {
+pub enum JsonValue {
     Null,
     Bool(bool),
     Number(f64),
     String(String),
     Array(Vec<JsonValue>),
     Object(HashMap<String, JsonValue>)
+}
+
+impl JsonValue {
+    fn fmt_with_indent(&self, f: &mut Formatter<'_>, indent: usize) -> fmt::Result {
+        let indent_str = "\t".repeat(indent);
+        match self {
+            JsonValue::Null => write!(f, "null"),
+            JsonValue::Bool(b) => write!(f, "{}", b),
+            JsonValue::Number(n) => write!(f, "{}", n),
+            JsonValue::String(s) => write!(f, "\"{}\"", s),
+            JsonValue::Array(a) => {
+                write!(f, "[")?;
+                for v in a.iter() {
+                    write!(f, "\n\t{}", indent_str)?;
+                    v.fmt_with_indent(f, indent + 1)?;
+                    write!(f, ",")?;
+                }
+                write!(f, "\n{}]", indent_str)
+            },
+            JsonValue::Object(o) => {
+                write!(f, "{{")?;
+                for (k, v) in o.iter() {
+                    write!(f, "\n\t{}{}: ", indent_str, k)?;
+                    v.fmt_with_indent(f, indent + 1)?;
+                    write!(f, ",")?;
+                }
+                write!(f, "\n{}}}", indent_str)
+            }
+        }
+    }
+}
+
+impl fmt::Display for JsonValue {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        self.fmt_with_indent(f, 0)
+    }
 }
 
 #[derive(Debug)]
@@ -28,7 +65,6 @@ pub enum Token {
     Number(f64),
     Boolean(bool),
 }
-
 
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
@@ -53,7 +89,7 @@ fn main() -> io::Result<()> {
             println!("\ntokens:\n{:?}", t);
             let object = parser::parse(t);
             match object {
-                Ok(v) => println!("\njson object:\n{:?}", v),
+                Ok(v) => println!("\njson object:\n{}", v),
                 Err(e) => println!("{}", e)
             }
 
